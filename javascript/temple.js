4db8bbd280ca5aef8ada0d656237e52dfcc1becc
akajprincess <46470952+akajprincess@users.noreply.github.com>
2019-04-12 19:13:59 -0600

    sdfgsadfg

---
+++ javascript/temple.js
@@ -0,0 +1,19 @@
+// let list = document.getElementsByTagName("title");
+// let weatherCode;
+// let jsonURLString;
+// let town = list[0].innerText;
+// if("Salt Lake Temple" === town) {
+//     weatherCode = 5604473;
+//     town = "preston";
+//     jsonURLString = 
+// } else if ("Soda Springs" === town) {
+//     weatherCode = 5678757;
+//     town = "sodasprings";
+// } else {
+//     weatherCode = 5585010;
+//     town = "fishhaven";
+// }
+// let weatherRequest = new XMLHttpRequest();
+// let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=" + weatherCode + "&units=imperial&APPID=ce65a8d7c7b2b3226cf302518813322f"
+// weatherRequest.open('Get', apiURLstring, true);
+// weatherRequest.send();
